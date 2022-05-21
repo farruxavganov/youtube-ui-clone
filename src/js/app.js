97a6c35fdf9baab7e8eed91e3fef5d1fedad2bc2
farrux <farrux.avganov@mail.ru>
2022-05-21 18:59:36 +0300

    finished navbar full responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,15 @@
+const mainHeadrStart = document.querySelector(".main-header__start");
+const body = document.querySelector("body");
+const mainHeaderStrtPointer = document.querySelector(".main-header__strt-pointer");
 const form = document.querySelector(".main-header__form");
 const inputBox = document.querySelector(".main-header__input-box");
 const input = document.querySelector(".main-header__input");
 const hidden = document.querySelector(".main-header__hidden");
+const smallSearch = document.querySelector(".main-header__small-search");
 const svgs = hidden.querySelectorAll("svg");
+const mainHeaderEnd = document.querySelector(".main-header__end");
+
+window.onresize = resizeControl;
 
 input.addEventListener("focus",inputFocus);
 
@@ -16,6 +23,10 @@ svgs[1].addEventListener("click", ()=>{
 })
 
 input.addEventListener("input",isClearValue);
+
+smallSearch.addEventListener("click",addSearchBar);
+
+mainHeaderStrtPointer.addEventListener("click",removeSearchBar);
 
 function inputFocus () {
 	form.style.borderLeft = "none";
@@ -36,8 +47,45 @@ function isClearValue () {
 		svgs[1].style.visibility = "visible";
 		svgs[1].style.opacity = "1";
 	}else {
-		svgs[1].style.background = "red";
 		svgs[1].style.visibility = "hidden";
 		svgs[1].style.opacity = "0";
+	}
+}
+
+function addSearchBar (){
+	mainHeadrStart.classList.add("d-none");
+	mainHeaderStrtPointer.classList.add("d-block");
+	form.classList.add("d-flex");
+	smallSearch.classList.add("d-none");
+	mainHeaderEnd.classList.add("d-none");
+	input.focus();
+}
+
+function removeSearchBar () {
+	mainHeadrStart.classList.remove("d-none");
+	mainHeaderStrtPointer.classList.remove("d-block");
+	form.classList.remove("d-flex");
+	smallSearch.classList.remove("d-none");
+	mainHeaderEnd.classList.remove("d-none");
+}
+
+function removeClass () {
+	mainHeadrStart.classList.remove("d-none");
+	mainHeaderStrtPointer.classList.remove("d-block");
+	form.classList.remove("d-flex");
+	smallSearch.classList.remove("d-none");
+	mainHeaderEnd.classList.remove("d-none");
+}
+
+function resizeControl () {
+	if(window.innerWidth >= 768) {
+		removeClass();
+	}else {
+		if(input == document.activeElement) {
+			addSearchBar();
+		}
+	}
+	if(input == document.activeElement){
+		input.focus();
 	}
 }
